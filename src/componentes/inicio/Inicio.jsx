14af0f42b8203abeb389a480e00f2f6b510fcdad
DinaRobles <marcerobles04@gmail.com>
2023-06-09 21:53:35 -0500

    inicio terminado

--- src/componentes/inicio/Inicio.jsx
+++ src/componentes/inicio/Inicio.jsx
@@ -3,9 +3,6 @@ import "./InicioStyles.css";
 function Inicio() {
   return (
     <>
-      <h1 className="subtitulo">
-        Hola me llamo <span className="nombre">Dina Robles</span>
-      </h1>
       <section className="container-inicio">
         <div className="foto">
           <img
@@ -14,14 +11,20 @@ function Inicio() {
           />
         </div>
         <div className="info">
+          <h1 className="subtitulo">
+            Hola me llamo <span className="nombre">Dina Robles ⭐</span>
+          </h1>
           <p className="textp">
             Me inscribí en este bootcamp para aprender acerca de la tecnologia y
-            adquirir nuevos conocimientos para mi ambiente laboral 🚀.
+            adquirir nuevos conocimientos para mi ambiente laboral.
           </p>
+          <p className="textp">🚀 Me apaciona la tecnología.</p>
+          <p className="textp">🤓 Me gusta investigar.</p>
+          <p className="textp">✌️ No me rindo nunca.</p>
         </div>
       </section>
       <section className="section-boton">
-        <a href="https://github.com/DinaRobles" className="boton">
+        <a href="https://github.com/DinaRobles" target="blank" className="boton">
           Visitar perfil Github
         </a>
       </section>
